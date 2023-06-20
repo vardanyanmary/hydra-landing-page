a9570c42d8d27511ca5f0ae34adb6bb1727364b6
Mary <mary.vardanyan.2001@gmail.com>
2023-06-20 22:36:40 +0400

    Header , Main , constants , variables

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,69 +3,50 @@ import michelangelo from "../../shared/assets/pexels-michelangelo-buonarroti-872
 import rectangle from "../../shared/assets/Rectangle 4.svg";
 import Button from "../../shared/ui/Button/Button";
 import arrow from "../../shared/assets/arrow-small-right.png";
-import location from "../../shared/assets/Combined-Shape.svg";
-import shape from "../../shared/assets/shape.svg";
-import shape1 from "../../shared/assets/shape1.svg";
+import { contactData } from "../../shared/constants/contactData";
 
 const Main = () => {
   return (
-    <main className={cls.Main}>
+    <main className={cls.main}>
       <article className={cls.article}>
         <section className={cls.sections}>
-          <div>
-            <p className={cls.dive}>
-              <span className={cls.span}> Dive </span> Into The Depths Of{" "}
-              <span className={cls.span}> Virtual Reality </span>
-            </p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut
-              earum doloremque quas vero tenetur id veritatis nostrum ratione
-              cum reprehenderit, error magnam cumque, ea dolorum vel, eveniet
-              quaerat voluptate? Quae?
-            </p>
-          </div>
+          <p className={cls.dive}>
+            <span className={cls.span}> Dive </span> Into The Depths Of{" "}
+            <span className={cls.span}> Virtual Reality </span>
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut earum
+            doloremque quas vero tenetur id veritatis nostrum ratione cum
+            reprehenderit, error magnam cumque, ea dolorum vel, eveniet quaerat
+            voluptate? Quae?
+          </p>
+
           <div className={cls.btnArr}>
             <Button state="Primary" type="button">
               BUILD YOUR WORLD
             </Button>
             <img src={arrow} alt="img" className={cls.arrow} />
           </div>
+
         </section>
 
         <section className={cls.sections}>
-        {/* <img src={rectangle} alt='rect' className={cls.rect}/>  */}
+          {/* <img src={rectangle} alt='rect' className={cls.rect}/>  */}
           <img src={michelangelo} alt="img" className={cls.img} />
         </section>
       </article>
 
       <article className={cls.PGC}>
-        <section className={cls.payGive}>
-          <img src={location} alt="locate" />
-          <div className={cls.Texts}>
-            <p className={cls.TextsP1}>Pay Us a Visit</p>
-            <p className={cls.TextsP2}>
-              Union St, Seattle,WA 98101 , United States
-            </p>
-          </div>
-        </section>
-        <section className={cls.payGive}>
-          <img src={shape} alt="locate" />
-          <div className={cls.Texts}>
-            <p className={cls.TextsP1}>Give Us a Call</p>
-            <p className={cls.TextsP2}>
-              (110) 1111 - 1010
-            </p>
-          </div>
-        </section>
-        <section className={cls.payGive}>
-          <img src={shape1} alt="locate" />
-          <div className={cls.Texts}>
-            <p className={cls.TextsP1}>Send Us a Message</p>
-            <p className={cls.TextsP2}>
-              Contact@HydraVTech.com
-            </p>
-          </div>
-        </section>
+        {contactData.map((data, index) => (
+          <section className={cls.payGive} key={index}>
+            <img src={data.icon} alt="locate" />
+            <div className={cls.Texts}>
+              <p className={cls.TextsP1}>{data.title}</p>
+              <p className={cls.TextsP2}>{data.description}</p>
+            </div>
+            {index !== contactData.length - 1 && <div className={cls.lines}></div>}
+          </section>
+        ))}
       </article>
     </main>
   );
